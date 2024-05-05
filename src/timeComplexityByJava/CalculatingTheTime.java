18aad125d9a0f5969e545f4b136c4d699ab168de
PratikVijaySonawane <pratiksonawane783@gmail.com>
2024-05-05 21:03:19 +0530

    Changes done

--- src/timeComplexityByJava/CalculatingTheTime.java
+++ src/timeComplexityByJava/CalculatingTheTime.java
@@ -1,5 +1,28 @@
 package timeComplexityByJava;
 
-public class CalculatingTheTime {
-
+public class CalculatingTheTime 
+{
+	public static void main(String[] args)
+	{
+		double now = System.currentTimeMillis();
+		CalculatingTheTime t1 = new CalculatingTheTime();
+		System.out.println("The Sum of (n = 99999) natural numbers"
+		                                                   +t1.findSum(99999));
+		System.out.println("Time taken--->"+(System.currentTimeMillis()-now)+
+				                                                  "in msecond");
+	}
+//	public int findSum(int n)
+//	{
+//		return n*(n+1)/2;
+//	}
+	public int findSum(int n)
+	{
+		int i,sum=0;
+		for(i=1;i<=n;i++)
+		{
+			sum=sum+i;	
+		}
+		return sum;
+	}
+	
 }
